072825cd3aef40c3ddc94379f46151c185c56ea2
TamasGalambos <tamas.galambos05@htl-salzburg.ac.at>
2023-04-26 17:50:42 +0200

    refac: magical string

--- Assets/MyGame/Scripts/AdventureGame.cs
+++ Assets/MyGame/Scripts/AdventureGame.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 
 public class AdventureGame : MonoBehaviour
 {
+    private const string stateInfoAlarm = "Info.Alarm";
 
     //private static readonly System.Random getrandom = new System.Random(123);
 
@@ -120,7 +121,7 @@ public class AdventureGame : MonoBehaviour
 
         }
 
-        if (nextState.name == "Info.Alarm")
+        if (nextState.name == stateInfoAlarm)
         {
             ResetCounters();
             Debug.Log("Counters Reseted + " + passedStatesCount + " " + collectedWoolCount + " " + dehydrationCount);
